035ee0ecca0e926b5550a65c353b093bb00416eb
Richard Klose <richard.klose@gmail.com>
2018-06-15 22:58:32 +0200

    Added a game view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,40 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 import {MatButtonModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
 import {RouterModule} from "@angular/router";
-import { GamelistComponent } from './gamelist/gamelist.component';
+import {GamelistComponent} from './gamelist/gamelist.component';
 import {GamesService} from "./games.service";
 import {HttpClientModule} from "@angular/common/http";
+import {GameviewComponent} from './gameview/gameview.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        GamelistComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AngularFontAwesomeModule,
-        MatToolbarModule,
-        MatSidenavModule,
-        MatButtonModule,
-        MatListModule,
-        MatInputModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                component: GamelistComponent
-            }
-        ])
-    ],
-    providers: [GamesService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    GamelistComponent,
+    GameviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularFontAwesomeModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: GamelistComponent
+      },
+      {
+        path: 'game/:id',
+        component: GameviewComponent
+      }
+    ])
+  ],
+  providers: [GamesService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
---
+++ src/app/gameview/gameview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'js-gameview',
+  templateUrl: './gameview.component.html',
+  styleUrls: ['./gameview.component.css']
+})
+export class GameviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
